a43e9e0402b76c9a70ef283a556ee03f00bb3199
risano <muhammadrisano@gmail.com>
2021-04-27 12:08:20 +0700

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react'
+import io from 'socket.io-client'
 
 function App() {
+  const [socket, setSocket] = useState(null)
+  const [message, setMessage] = useState('')
+  const [messages, setMessages] = useState([])
+  const setupSocket = ()=>{
+    console.log('hello');
+    const newSocket = io("http://localhost:4567")
+    console.log(newSocket);
+    newSocket.on("connect", ()=>{
+      console.log('connect');
+    })
+    setSocket(newSocket)
+  }
+  useEffect(()=>{
+    setupSocket()
+    
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  },[])
+  
+  useEffect(() => {
+    if (socket){
+      socket.on('recMessage', (data) => {
+        // const dataMessage = messages
+        // dataMessage.push(data)
+
+        setMessages([...messages, data])
+      })
+    }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [socket, messages])
+
+  const handleClick = ()=>{
+    socket.emit('kirimMessage', message)
+    setMessage('')
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div>
+    <h1>halaman utama</h1>
+    <ul>
+      {messages.map((item, index)=>
+        <li key={index}>{item}</li>
+      )}
+    </ul>
+      <input type="text" name="message" id="message" value={message} placeholder="Kirim Message" onChange={(e) => setMessage(e.target.value)}/>
+      <button onClick={handleClick}>kirim message</button>
+  </div>
   );
 }
 
